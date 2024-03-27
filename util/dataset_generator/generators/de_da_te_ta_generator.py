adbca6099ea6a901394f124c9af012914ac1b331
burak <rburaksaritas@gmail.com>
2024-03-27 21:36:24 +0300

    fix bugs

--- util/dataset_generator/generators/de_da_te_ta_generator.py
+++ util/dataset_generator/generators/de_da_te_ta_generator.py
@@ -7,17 +7,23 @@ def find_suffix_indices(sentence):
     suffix_indices = [(match.start(3), match.end(3)) for match in matches]
     return suffix_indices, sentence
 
+def transform_suffix(sentence, start, end):
+    suffix = sentence[start:end]
+    if sentence[start - 1] == ' ':
+        # If "{prefix}{whitespace}{suffix}" format, convert to "{prefix}{suffix}"
+        sentence = sentence[:start-1] + suffix + sentence[end:]
+    else:
+        # If "{prefix}{suffix}" format, convert to "{prefix}{whitespace}{suffix}"
+        sentence = sentence[:start] + ' ' + suffix + sentence[end:] 
+    return sentence
+
 def transform_sentence(sentence, suffix_indices):
     transformed_sentence = sentence
     for start, end in reversed(suffix_indices):
-        suffix = transformed_sentence[start:end]
-        print("start", "-" + transformed_sentence[:start-1] + "-", "suffix:", "-" + suffix + "-", "end:", "-" + transformed_sentence[end:] + "-")
-        if transformed_sentence[start - 1] == ' ':
-            # If "{prefix}{whitespace}{suffix}" format, convert to "{prefix}{suffix}"
-            transformed_sentence = transformed_sentence[:start-1] + suffix + transformed_sentence[end:]
-        else:
-            # If "{prefix}{suffix}" format, convert to "{prefix}{whitespace}{suffix}"
-            transformed_sentence = transformed_sentence[:start] + ' ' + suffix + transformed_sentence[end:]
+        # Randomly choose whether to transform the suffix. 
+        # Uncomment below if you want untransformed examples as well.
+        # if (random.choice([True, False])):
+            transformed_sentence = transform_suffix(transformed_sentence, start, end)
     return transformed_sentence
 
 def process_sentences(sentences):
